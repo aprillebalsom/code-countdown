552e2e559c4b1330de5d046f076652b39023de55
aprillebalsom <aprillebalsom@gmail.com>
2020-09-16 17:06:14 -0400

    added js script and psuedo code

---
+++ script.js
@@ -0,0 +1,27 @@
+//THE CODE
+
+//create app to hold all methods
+const app = {};
+
+//event listener for when the user chooses a mood from a dropdown menu
+//store the information in a variable and pass it as a agurment into the api call
+app.userSelection = function () { };
+
+//make ajax request and return the inspirational quote based on user's selection
+app.ajaxCall = function (mood) { };
+
+//display quote to the page
+//display a "pick another mood" button allowing the user to select another mood and try again 
+app.displayQuote = function () { };
+
+//set time interval to countdown and ".html()" the number based on user selection
+app.clock = function () { };
+
+//display a "times up" alert for when the countdown timer is up
+app.timesUp = function () { };
+
+//init function that kicks off the app
+app.init = function () { };
+
+//document ready function
+$(function () { });
